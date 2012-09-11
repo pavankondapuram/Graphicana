621f41e2b96939272d4bf54c358b8c3b22098f65
graphicana <pavan@graphicana>
2012-09-11 15:44:32 +0530

    new beginning

--- animate.js
+++
@@ -1,9 +0,0 @@
-$.fn.loopingAnimation = function(props, dur, eas){
-    if( this.data('loop') == true ){
-       this.animate( props, dur, eas, function(){
-           if( $(this).data('loop') == true ) 
-			$(this).loopingAnimation(props, dur, eas);
-       }
-	}
-    return this; // Don't break the chain
-}
