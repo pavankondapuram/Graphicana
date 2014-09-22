dde653bd8e27647187560572beb90d45abfb0413
graphicana <pavan_sry@yahoo.co.in>
2014-09-22 18:34:04 +0530

    adding google analytics

--- index.php
+++ index.php
@@ -18,7 +18,16 @@
     <link href="css/style.css" rel="stylesheet"/>
     <link href="css/color_cheme.css" rel="stylesheet"/>
     <link href="css/responsive.css" rel="stylesheet"/>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+        ga('create', 'UA-55035849-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
     <script src="js/jquery-1.10.2.min.js"></script>
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
